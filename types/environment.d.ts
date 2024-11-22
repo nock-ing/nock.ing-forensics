478c48c9600cce68fae931ae980e037cd9d6e946
ALsJourney <63744576+ALsJourney@users.noreply.github.com>
2024-11-22 23:10:04 +0100

    Added prettier

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -6,6 +6,27 @@ declare global {
             BITCOIN_RPC_PASSWORD: string;
         }
     }
+  interface BlockchainInfo {
+    chain: string
+    blocks: number
+    headers: number
+    bestblockhash: string
+    difficulty: number
+    mediantime: number
+    verificationprogress: number
+    initialblockdownload: boolean
+    chainwork: string
+    size_on_disk: number
+    pruned: boolean
+    warnings: string
 }
 
+interface RecentBlock {
+    height: number
+    hash: string
+    time: number
+    txcount: number
+} 
+}
+  
 export {}
