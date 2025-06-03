8708f75f1b9a41952a1b5bf7ed93c8975205a76e
AL <63744576+ALsJourney@users.noreply.github.com>
2025-06-03 16:01:59 +0200

    Bun setup with better auth, testing to deploy on coolify.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,53 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import "dotenv/config";
+import {auth, configuredProviders} from "./auth";
 
-const app = new Hono()
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/", (c) => {
+    return c.json({
+        message: "Hello Hono x Better Auth!",
+        description:
+            "This is a simple example of a Hono x Better Auth application, you can use it as a starting point for your own application.",
+        links: [
+            {
+                text: "Go to the Authentication API Documentation",
+                href: new URL("/api/auth/reference", c.req.url).href,
+            },
+        ],
+    });
+});
+
+app.use(
+    "/api/auth/**", // or replace with "*" to enable cors for all routes
+    cors({
+        origin: (origin, _) => {
+            if (allowedOrigins.includes(origin)) {
+                return origin;
+            }
+            return undefined;
+        },
+        allowHeaders: ["Content-Type", "Authorization"],
+        allowMethods: ["POST", "GET", "OPTIONS"],
+        exposeHeaders: ["Content-Length"],
+        maxAge: 600,
+        credentials: true,
+    }),
+);
+
+app.on(["GET"], "/api/auth-providers", (c) => {
+    return c.json(Object.keys(configuredProviders));
+});
+
+app.on(["POST", "GET"], "/api/auth/**", (c) => {
+    return auth.handler(c.req.raw);
+});
+
+export default {
+    port: process.env.APP_PORT || 8558,
+    host: process.env.APP_HOST || undefined,
+    fetch: app.fetch,
+};
